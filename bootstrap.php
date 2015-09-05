9c470d1f4776e9aefefc42f44885083bddcd1701
tourze <25803471@qq.com>
2015-09-05 17:35:27 +0800

    1. 更正依赖 2. 增加一个常量，用于识别当前组件的路径

--- bootstrap.php
+++ bootstrap.php
@@ -4,3 +4,8 @@ if (is_file(__DIR__ . '/vendor/autoload.php'))
 {
     require_once __DIR__ . '/vendor/autoload.php';
 }
+
+if ( ! defined('TOURZE_FLOW_DIR'))
+{
+    define('TOURZE_FLOW_DIR', __DIR__ . DIRECTORY_SEPARATOR);
+}
